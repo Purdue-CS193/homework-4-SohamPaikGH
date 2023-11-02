4ef9d3063848a2f72e22b592a3029bab3a9ea0fe
Soham Paik <soham.paik@gmail.com>
2023-11-02 17:56:23 -0400

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -9,25 +9,25 @@ public class Test1 {
     public static double basicCalculator(Operation operation, int num1, int num2){
         double result = 0;
 	
-	      if ( operation == Operation.ADD ) {
+	if ( operation == Operation.ADD ) {
 		      result = num1 + num2;
         } 
       
         if ( operation == Operation.SUBTRACT ) {
 		      result = num1 - num2;
-	      }
+	}
 
-	      if ( operation == Operation.MULTIPLY ) {
+	if ( operation == Operation.MULTIPLY ) {
 		      result = num1 * num2;
-	      }
+	}
 
-	      if ( operation == Operation.DIVIDE ) {
-		      if ( num2 == 0 ) {
-			      System.out.println("can't divide by 0");
-		      } else {
-			      result = ((double)num1) / num2;
-		      }
-	      }
+	if ( operation == Operation.DIVIDE ) {
+		if ( num2 == 0 ) {
+			System.out.println("can't divide by 0");
+		} else {
+			result = ((double)num1) / num2;
+		}
+	}
 
         return result;
     }
